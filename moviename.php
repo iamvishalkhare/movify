a83399585e8e3af9d0fa73a220f6e2587396b7a2
Vishal Khare <vishalkhare39@gmail.com>
2018-08-21 18:42:38 +0530

    Adding API Key

--- moviename.php
+++ moviename.php
@@ -1,6 +1,6 @@
 <?php
 $moviename=$_GET['moviee'];
-$json = file_get_contents('http://www.omdbapi.com/?t='.$moviename.'&plot=full');
+$json = file_get_contents('http://www.omdbapi.com/?t='.$moviename.'&plot=full&apikey=37b3bb53');
 //$obj = json_decode($json);
 echo $json;
 ?>
